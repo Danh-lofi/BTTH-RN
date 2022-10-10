79c288dee88ab9882259d4fd2441455781aaedde
danh <thdanh2001@gmail.com>
2022-10-10 09:35:13 +0700

    .

---
+++ BTTH/onthigk/AwesomeProject/App.js
@@ -0,0 +1,105 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Item from "./conponents/Item";
+
+export default function App() {
+  const [list, setList] = useState([]);
+  const [item, setItem] = useState("");
+
+  const submitItemHandle = () => {
+    if (item) {
+      setList((list) => [...list, item]);
+      setItem("");
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <View
+        style={[styles.textCenter, { height: "10%", flex: 1, width: "100%" }]}
+      >
+        <Text style={[styles.textWhite, styles.textCenter, styles.fontBold]}>
+          ToDo App
+        </Text>
+      </View>
+      <View
+        style={[
+          styles.pd,
+          { height: "10%", width: "100%", position: "relative" },
+        ]}
+      >
+        <TextInput
+          value={item}
+          onChangeText={setItem}
+          style={{
+            borderRadius: 30,
+            height: 35,
+            width: "100%",
+            backgroundColor: "#fff",
+            paddingHorizontal: 20,
+            fontSize: 10,
+            paddingRight: 50,
+          }}
+        />
+        <TouchableOpacity
+          onPress={submitItemHandle}
+          style={[
+            {
+              width: 50,
+              height: 35,
+              borderRadius: 30,
+              backgroundColor: "#61dafb",
+              position: "absolute",
+              right: 40,
+              top: 10,
+            },
+            styles.textCenter,
+          ]}
+        >
+          <Text style={[{}, styles.textWhite, styles.fontBold]}>&gt;</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={[styles.pd, { height: "80%", width: "100%" }]}>
+        <View style={{}}>
+          <FlatList
+            data={list}
+            renderItem={({ item, index }) => {
+              return <Item key={index} id={index} item={item} />;
+            }}
+          ></FlatList>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  fontBold: {
+    fontWeight: "bold",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#333",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 20,
+  },
+  textCenter: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textWhite: {
+    color: "#fff",
+  },
+  pd: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+  },
+});
